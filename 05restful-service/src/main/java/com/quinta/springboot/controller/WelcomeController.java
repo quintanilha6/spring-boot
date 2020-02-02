236c099d3fb96455006dbd1a2802207e6c7446a1
João Quintanilha <jpquintanilha6@gmail.com>
2020-02-02 10:23:37 +0000

    add properties in yaml format and declare global vars

---
+++ 05restful-service/src/main/java/com/quinta/springboot/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package com.quinta.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+	
+	
+	/***********
+	*	GETS   *
+	***********/
+	@GetMapping("/welcome")
+	public String retrieveWelcomeMessage() {
+		return welcomeMessage;
+	}
+}
